223e3951660419d3a538edbec64478c861f6c620
cromz22 <tt.pfr4343@gmail.com>
2021-10-27 10:33:22 +0900

    save file

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+import json
+import base64
 
 app = FastAPI()
 
@@ -21,5 +22,13 @@ async def read_root() -> dict:
 
 @app.post("/save-audio")
 async def save_audio(taskjson: dict) -> dict:
-    print(taskjson)
+    # print(taskjson)
+    # print(type(taskjson))
+    for elem in taskjson:
+        blobs = taskjson[elem]
+        for blob in blobs:
+            print(blob)
+            with open("tmp.wav", "wb") as f:
+                f.write(base64.b64decode(blob))
     return taskjson
+    # return {"message": "connected to backend"}
